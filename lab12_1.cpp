02619e4444ca95c5e3d4a18b45cc13190baae38e
ZenPtc <maku4fb@gmail.com>
2020-02-07 16:10:13 +0700

    12.1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -18,3 +18,15 @@ int main()
 }
 
 //Write definition of stat() here 
+void stat(const double A[],int N,double B[]){
+    double sum=0, sum2=0;
+    B[2]=B[3]=A[0];
+    for(int i=0;i<N;i++){
+        sum += A[i];
+        sum2 += pow(A[i],2);
+        if(A[i]>B[2]) B[2] = A[i];
+        if(A[i]<B[3]) B[3] = A[i];
+    }
+    B[0] = sum/N;
+    B[1] = sqrt((sum2/N)-pow(B[0],2));
+}
